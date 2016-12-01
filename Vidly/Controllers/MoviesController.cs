f6c2763069164da28559ec598bbc3aef05521501
Amruth Pillai <im.amruth@gmail.com>
2016-12-02 02:08:37 +0530

    different ways of passing views

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -14,20 +14,24 @@ namespace Vidly.Controllers
         {
             var movie = new Movie() { Name = "Inception" };
 
+            /* ViewData["Movie"] = movie; */
+
             return View(movie);
+
             // return Content("Hello World!");
             // return HttpNotFound();
             // return new EmptyResult();
             // return RedirectToAction("Index", "Home", new { page = 1, sortBy = "Name"});
         }
 
+        // GET: Movies/Released/{Year}/{Month}
         [Route("movies/released/{year:regex(\\d{4})}/{month:regex(\\d{2}):range(1,12)")] 
         public ActionResult ByReleaseDate(int year, int month)
         {
             return Content(year + "/" + month);
         }
 
-        // GET: Movies/Edit/:ID
+        // GET: Movies/Edit/{ID}
         public ActionResult Edit(int id)
         {
             return Content("Movie ID = " + id);
